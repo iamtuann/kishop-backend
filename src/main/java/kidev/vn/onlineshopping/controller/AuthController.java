ec461dfb84d88eabf2559a3b05d2d6572347616b
nguyentuan <dungtuann67@gmail.com>
2024-09-10 11:53:16 +0700

    update api sign up

--- src/main/java/kidev/vn/onlineshopping/controller/AuthController.java
+++ src/main/java/kidev/vn/onlineshopping/controller/AuthController.java
@@ -3,14 +3,11 @@ package kidev.vn.onlineshopping.controller;
 import kidev.vn.onlineshopping.Constants;
 import kidev.vn.onlineshopping.config.security.jwt.JwtUtils;
 import kidev.vn.onlineshopping.config.security.service.UserDetailsImpl;
-import kidev.vn.onlineshopping.entity.AuthUser;
 import kidev.vn.onlineshopping.model.CommonResponse;
-import kidev.vn.onlineshopping.model.authUser.AuthUserModel;
 import kidev.vn.onlineshopping.model.authUser.AuthUserRequest;
 import kidev.vn.onlineshopping.model.authUser.AuthUserResponse;
 import kidev.vn.onlineshopping.model.authUser.LoginRequest;
 import kidev.vn.onlineshopping.service.AuthUserService;
-import kidev.vn.onlineshopping.service.RoleService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,20 +21,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-
-    private final RoleService roleService;
 
     private final AuthUserService authUserService;
 
@@ -46,34 +36,42 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
-    private ResponseEntity<CommonResponse<AuthUserModel>> createUser(@RequestBody AuthUserRequest request, UriComponentsBuilder ucb) {
-        CommonResponse<AuthUserModel> response = new CommonResponse<>();
+    private ResponseEntity<CommonResponse<AuthUserResponse>> createUser(@RequestBody AuthUserRequest request) {
+        CommonResponse<AuthUserResponse> response = new CommonResponse<>();
         try {
             if (authUserService.existsByEmail(request.getEmail())) {
                 response.setStatusCode(Constants.RestApiReturnCode.BAD_REQUEST);
-                response.setMessage(Constants.RestApiReturnCode.BAD_REQUEST_TXT);
+                response.setMessage("Tài khoản email đã được đăng ký");
                 response.setOutput(null);
-                response.setError("Email is existed");
-                return ResponseEntity.badRequest().body(response);
+                response.setError(Constants.RestApiReturnCode.BAD_REQUEST_TXT);
+                return ResponseEntity.ok(response);
             } else {
-                AuthUserModel output = authUserService.create(request);
-                URI locationOfNewCashCard = ucb
-                        .path("api/users/{id}")
-                                .buildAndExpand(output.getId())
-                                        .toUri();
-                response.setStatusCode(Constants.RestApiReturnCode.SUCCESS);
-                response.setMessage(Constants.RestApiReturnCode.SUCCESS_TXT);
-                response.setOutput(output);
-                return ResponseEntity.created(locationOfNewCashCard).body(response);
+                authUserService.create(request);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+                Authentication authentication = authenticationManager.authenticate(authenticationToken);
+                setAuthentication(response, authentication);
+                return ResponseEntity.ok(response);
             }
         } catch (Exception e) {
             response.setStatusCode(Constants.RestApiReturnCode.SYS_ERROR);
-            response.setMessage(Constants.RestApiReturnCode.SYS_ERROR_TXT);
+            response.setMessage("Có lỗi xảy ra");
             response.setOutput(null);
-            response.setError("Có lỗi xảy ra");
+            response.setError(Constants.RestApiReturnCode.SYS_ERROR_TXT);
             logger.error("createUser ", e);
             return ResponseEntity.status(500).body(response);
         }
+    }
+
+    private void setAuthentication(CommonResponse<AuthUserResponse> response, Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwtCookie = jwtUtils.generateJwtToken(userDetails);
+        AuthUserResponse userInfo = new AuthUserResponse(userDetails, jwtCookie);
+
+        response.setStatusCode(Constants.RestApiReturnCode.SUCCESS);
+        response.setMessage(Constants.RestApiReturnCode.SUCCESS_TXT);
+        response.setOutput(userInfo);
     }
 
     @PostMapping("/login")
@@ -89,15 +87,7 @@ public class AuthController {
             Authentication authentication = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
             if (authentication.isAuthenticated()) {
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-
-                UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-                String jwtCookie = jwtUtils.generateJwtToken(userDetails);
-                AuthUserResponse userInfo = new AuthUserResponse(userDetails, jwtCookie);
-                response.setStatusCode(Constants.RestApiReturnCode.SUCCESS);
-                response.setMessage(Constants.RestApiReturnCode.SUCCESS_TXT);
-                response.setOutput(userInfo);
+                setAuthentication(response, authentication);
 
             } else {
                 response.setStatusCode(Constants.RestApiReturnCode.UNAUTHORIZED);
@@ -111,9 +101,9 @@ public class AuthController {
             return ResponseEntity.ok().body(response);
         } catch (Exception e) {
             response.setStatusCode(Constants.RestApiReturnCode.SYS_ERROR);
-            response.setMessage(Constants.RestApiReturnCode.SYS_ERROR_TXT);
+            response.setMessage("Có lỗi xảy ra");
             response.setOutput(null);
-            response.setError("Có lỗi xảy ra");
+            response.setError(Constants.RestApiReturnCode.SYS_ERROR_TXT);
             logger.error("login ", e);
             return ResponseEntity.status(500).body(response);
         }
